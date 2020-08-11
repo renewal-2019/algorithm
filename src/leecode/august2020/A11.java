e5c0f142abdbb7f5d703cc3414b3f76355aa26a7
renewal-2019 <swiftzsl@163.com>
2020-08-11 20:11:46 +0800

    判断二叉树相等(深度优先算法)

---
+++ src/leecode/august2020/A11.java
@@ -0,0 +1,47 @@
+package leecode.august2020;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ */
+public class A11 {
+
+    /**
+     * 深度优先算法
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
