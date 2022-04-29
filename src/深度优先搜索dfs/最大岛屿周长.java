cd5f54b5b1770e89379f56b1ccd76c1f10330b12
renewal-2019 <swiftzsl@163.com>
2022-04-29 21:17:28 +0800

    dfs：求岛屿最大周长

---
+++ src/深度优先搜索dfs/最大岛屿周长.java
@@ -0,0 +1,53 @@
+package 深度优先搜索dfs;
+
+public class 最大岛屿周长 {
+    public static void main(String[] args) {
+        int[][] island = {
+                {0, 1, 0, 1, 0},
+                {1, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1}
+        };
+        System.out.println(islandPerimeter(island));
+    }
+
+    public static int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
+                    // 题目限制只有一个岛屿，计算一个即可
+                    int dfs = dfs(grid, r, c);
+                    res = Math.max(dfs, res);
+                }
+            }
+        }
+        return res;
+    }
+
+    static int dfs(int[][] grid, int r, int c) {
+        // 函数因为「坐标 (r, c) 超出网格范围」返回，对应一条黄色的边
+        if (!inArea(grid, r, c)) {
+            return 1;
+        }
+        // 函数因为「当前格子是海洋格子」返回，对应一条蓝色的边
+        if (grid[r][c] == 0) {
+            return 1;
+        }
+        // 函数因为「当前格子是已遍历的陆地格子」返回，和周长没关系
+        if (grid[r][c] == 2) {
+            return 0;
+        }
+        grid[r][c] = 2;
+        return dfs(grid, r - 1, c)
+                + dfs(grid, r + 1, c)
+                + dfs(grid, r, c - 1)
+                + dfs(grid, r, c + 1);
+    }
+
+    // 判断坐标 (r, c) 是否在网格中
+    static boolean inArea(int[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length
+                && 0 <= c && c < grid[0].length;
+    }
+}
