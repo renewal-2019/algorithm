3c5c4ba39cfa7072b27b40fa32285f9079415505
renewal-2019 <swiftzsl@163.com>
2022-05-27 20:56:45 +0800

    二分法：变种,根据条件进行二分

---
+++ src/二分法/数组中缺失的数字.java
@@ -0,0 +1,20 @@
+package 二分法;
+
+/**
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ */
+public class 数组中缺失的数字 {
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
