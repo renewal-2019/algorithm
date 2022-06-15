0721874851c1f77da9610c91ae6ec3426195430b
renewal-2019 <swiftzsl@163.com>
2022-06-15 22:14:57 +0800

    双指针操作链表

---
+++ src/双指针/链表的公共节点.java
@@ -0,0 +1,17 @@
+package 双指针;
+
+public class 链表的公共节点 {
+    // https://leetcode.cn/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/solution/liang-ge-lian-biao-de-di-yi-ge-gong-gong-pzbs/
+    // 简单解法是使用哈希表
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
